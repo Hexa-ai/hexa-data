4b3d760491be2a12f727f29fd6ecac3343f92098
MaximeMRF <maxime.roth-fessler@epitech.eu>
2023-03-17 17:37:09 +0100

    fix: update path in the install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 # Installation script for Hexa-Data
 
-# Run this command to install Hexa-Data
 # curl -sSL https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/install.sh | bash
 
-# Script begin here
-
 # Get the .env file
-curl -o .env https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/.env.install
+curl -o .env https://raw.githubusercontent.com/Hexa-ai/hexa-data/master/.env.install
 
 # Get the conf file for warpfleet
-curl -O https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/conf.json
+curl -O https://raw.githubusercontent.com/Hexa-ai/hexa-data/master/conf.json
 
 # Get the dockerfile for warpfleet
-curl -O https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/dockerfile-wfs
+curl -O https://raw.githubusercontent.com/Hexa-ai/hexa-data/master/dockerfile-wfs
 
 # Get the docker-compose file
-curl -O https://raw.githubusercontent.com/MaximeMRF/test-autoinstall/main/docker-compose.yml
+curl -O https://raw.githubusercontent.com/Hexa-ai/hexa-data/master/docker-compose.yml
 
 # Run the docker-compose file in background
 docker-compose up -d
