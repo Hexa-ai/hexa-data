f40b6c5f4ced7cbe4ee1ffffcf79054522f920dc
Alexis Bouchez <bouchezalexis1@gmail.com>
2023-05-03 17:41:31 +0200

    feat: show triggerType, when the variable is of type boolean

--- ui/src/pages/Tags/Variables/show.vue
+++ ui/src/pages/Tags/Variables/show.vue
@@ -183,7 +183,7 @@
               </div>
               <div
                 class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6"
-                v-if="showTresholdField"
+                v-if="refTag!.valueType == 1"
               >
                 <div class="sm:col-span-2">
                   <InputField
