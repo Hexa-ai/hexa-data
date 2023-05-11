333c29f4ed3da1a4ca031c28fac79fb916f51a6b
Julien TALBOURDET <julien.talbourdet@gmail.com>
2023-05-11 21:42:33 +0200

    fix:set browser zoom to 0.8

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -113,9 +113,6 @@ const doc = document.documentElement;
 // Fonction pour définir le niveau de zoom
 function setZoom(zoom:number) {
   doc.style.zoom = zoom;
-  // doc.style.webkitTransform = `scale(${zoom})`;
-  // doc.style.msTransform = `scale(${zoom})`;
-  // doc.style.transform = `scale(${zoom})`;
  }
 
 setZoom(0.88);
