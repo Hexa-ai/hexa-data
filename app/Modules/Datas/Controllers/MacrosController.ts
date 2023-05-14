da59da8b495c278aa68ff38cfc4e5399581e1b6f
Julien TALBOURDET <julien.talbourdet@gmail.com>
2023-05-14 21:28:05 +0200

    fix:macro exec project id path replacement

--- app/Modules/Datas/Controllers/MacrosController.ts
+++ app/Modules/Datas/Controllers/MacrosController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Tag from "../Models/Tag"
+import Warp10Service from 'App/Services/Warp10Service'
 
 export default class MacrosController {
   /**
@@ -12,26 +13,14 @@ export default class MacrosController {
   public async getMacro({params, response}: HttpContextContract) {
     let macroName: string = params.macroName.slice(0, -4)
     let projectId: number = Number(params.projectId)
-    try {
-      let macro = await Tag.query()
+
+    let macro = await Tag.query()
       .where('projectId', projectId)
       .andWhere('name',macroName)
       .andWhere('type',3)
       .andWhere('scriptInterval',0)
-      .first()
-      if ((macro!=null)) {
-        return response.type('text/plain; charset=utf-8').send(macro.script)
-      } else {
-        macro = await Tag.query()
-        .andWhere('name',macroName)
-        .andWhere('type',3)
-        .andWhere('scriptInterval',0)
-        .firstOrFail()
-        return response.type('text/plain; charset=utf-8').send(macro.script)
-      }
-    } catch {
-      return response.status(404)
-    }
-
+      .firstOrFail()
+    macro.script= Warp10Service.wsReplaceMacroProjectPrefixe(macro.script,projectId)
+    return response.type('text/plain; charset=utf-8').send(macro.script)
   }
 }
