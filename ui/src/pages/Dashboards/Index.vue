b57b43e69aa7e02435911fd451ce713f7007ed76
Anael Favre <anael.favre@gmail.com>
2023-11-23 11:47:38 +0100

    feat: frontend implementation of grafana

--- ui/src/pages/Dashboards/Index.vue
+++ ui/src/pages/Dashboards/Index.vue
@@ -1,15 +1,14 @@
 <template>
-  <IndexVersion1 v-if="project?.dashboardVersion === 1"></IndexVersion1>
-  <IndexVersion2 v-else-if="project?.dashboardVersion === 2" :project="project"></IndexVersion2>
+  <IndexLegacy v-if="project?.dashboardType === 'LEGACY'"></IndexLegacy>
+  <IndexGrafana v-else-if="project?.dashboardType === 'GRAFANA'" :project="project"></IndexGrafana>
 </template>
 
 <script setup lang="ts">
 import { inject, ref } from 'vue'
 import Store from '../../store/Store'
-import IndexVersion1 from './Index/V1.vue'
-import IndexVersion2 from './Index/V2.vue'
+import IndexLegacy from './Index/Legacy.vue'
+import IndexGrafana from './Index/Grafana.vue'
 
 const store: Store = inject('store')!
-
 const project = ref(store.currentProject)
 </script>
