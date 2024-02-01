3b6e7090c4b4139a27923d8c727f08835450a7f3
Anael Favre <anael.favre@gmail.com>
2024-02-01 10:47:56 +0100

    fix: bad type display on variable listing

--- ui/src/pages/Tags/Variables/Index/Warp10.vue
+++ ui/src/pages/Tags/Variables/Index/Warp10.vue
@@ -126,10 +126,9 @@
                 <template v-else> {{  $t('tags.noLastValue') }} </template>
               </div>
 
-              <div class="flex flex-col justify-center px-5 hidden md:block">
+              <div class="flex flex-col justify-center px-5 hidden md:block" style="min-width: 110px;">
                 <div>
-                  <Tag v-if="item.c.includes('.@')" severity="info">string / bool</Tag>
-                  <Tag v-else severity="info">numeric</Tag>
+                  <Tag v-if="item.l.type" severity="info">{{ item.l.type }}</Tag>
                 </div>
                 <div v-if="item.l['unit']" class="mt-1">
                   <Tag :value="item.l['unit']"></Tag>
